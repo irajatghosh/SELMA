c5fe28881c3710290cdd85ea90692ee509d6e5b3
Rajat Ghosh <rajatghosh52@gmail.com>
2021-10-16 13:55:01 +0200

    Added header.

---
+++ src/components/header/NavigationLists.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <v-btn text class="mr-2">
+      About
+    </v-btn>
+    <v-btn rounded depressed class="pink lighten-1 mr-2">
+      Login
+    </v-btn>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/header/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar dense app color="primary" dark>
+    <div class="d-flex align-center">
+      <h2 class="text-uppercase ">Selma</h2>
+    </div>
+
+    <v-spacer></v-spacer>
+    <navigation-lists></navigation-lists>
+  </v-app-bar>
+</template>
+
+<script>
+import NavigationLists from "./NavigationLists.vue";
+export default {
+  components: {
+    NavigationLists,
+  },
+};
+</script>
+
+<style></style>
